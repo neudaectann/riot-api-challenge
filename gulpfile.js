13ca1f80ab5d27a6444ab023b0f951315c6027a1
Timur Urazalinov <urazalinov.timur@hotmail.com>
2015-08-28 14:57:01 +0600

    A hella lot of changes here.
    - Finished landing page
    - Removed unused Polymer dependencies
    - Added Angular js
    - Added improvements to gulpfile: its now uses javascript minification
    and plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp        = require("gulp"),
     minifyHTML  = require("gulp-minify-html"),
     sass        = require("gulp-ruby-sass"),
+    plumber     = require("gulp-plumber"),
+    uglify      = require("gulp-uglify"),
+    prefix      = require("gulp-autoprefixer"),
     // imagemin    = require("gulp-imagemin"),
     browserSync = require("browser-sync");
 
 // gulp config - defines paths for html, css, etc.
-//
 var config = {
     paths:{
         html:{
@@ -26,7 +28,7 @@ var config = {
             dest: "build/js/"
         },
         img:{
-            src: "application/img/**/*",
+            src: "application/img/*.*",
             dest: "build/img/"
         }
     }
@@ -34,46 +36,49 @@ var config = {
 
 // task for sass compile
 // gulp-ruby-sass: 1.x
-//
-gulp.task("sass", function(){
+gulp.task("sass", function() {
     return sass(config.paths.sass.src, {
                     style: "compressed",
                     emitCompileError: true
                 })
-                .on("error", function(err) {
-                    sass.logError(err);
-                    process.exit(1);
-                })
+                .pipe(plumber())
+                .pipe(prefix("last 2 versions"))
                 .pipe(gulp.dest(config.paths.sass.dest))
                 .pipe(browserSync.reload({stream: true}));
-})
+});
+
+gulp.task("js-min", function() {
+    return gulp.src(config.paths.js.src)
+                .pipe(plumber())
+                .pipe(uglify())
+                .pipe(gulp.dest(config.paths.js.dest))
+                .pipe(browserSync.reload({stream: true}));
+});
 
 // Minify HTML task.
 // It get all .html files from application directory and store them into build directory after minification
-//
 gulp.task("html", function() {
     return gulp.src(config.paths.html.src)
+                .pipe(plumber())
                 .pipe(minifyHTML())
                 .pipe(gulp.dest(config.paths.html.dest))
                 .pipe(browserSync.reload({stream: true}));
 });
 
 // copy bower dependencies to build output folder
-//
 gulp.task("bower-copy", function() {
     return gulp.src(config.paths.bower.src).pipe(gulp.dest(config.paths.bower.dest));
 });
 
 // compress images
-//
 gulp.task("img-compress", function() {
     return gulp.src(config.paths.img.src)
-                //.pipe(imagemin())
-                .pipe(gulp.dest(config.paths.img.dest));
+                // .pipe(imagemin())
+                .pipe(gulp.dest(config.paths.img.dest))
+                .pipe(browserSync.reload({stream: true}));
 });
 
 // BrowserSync task for livereload functionality
-//
 gulp.task("browser-sync", function() {
     browserSync({
         server: {
@@ -82,16 +87,15 @@ gulp.task("browser-sync", function() {
     })
 })
 
-// build task - compile sass, minify html/javascript, copy bower dependencies
-//
-gulp.task("build", ["sass", "html", "img-compress"]);
+// build task - compile sass, minify html/javascript, compress images copy bower dependencies
+gulp.task("build", ["sass", "js-min", "html", "img-compress", "bower-copy"]);
 
 // serve task - runs loval server environment
-//
 gulp.task("serve", ["build", "browser-sync"], function() {
-    gulp.watch(config.paths.bower.src, ["bower_copy"]);
     gulp.watch(config.paths.html.src, ["html", browserSync.reload]);
     gulp.watch(config.paths.sass.srcWatch, ["sass", browserSync.reload]);
+    gulp.watch(config.paths.js.srcWatch, ["js", browserSync.reload]);
+    gulp.watch(config.paths.img.srcWatch, ["img-compress", browserSync.reload]);
 });
 
 // TODO: deploy task - copy all files from ./build to remote server via ssh
